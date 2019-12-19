0184fe1d970d2d923fcb6eb92459886efbff1bca
Chikako Takasaki <a3x07.byd.chika@gmail.com>
2019-12-19 15:25:41 +0900

    add s10_v2 nn

--- keras/nn/run_100.sh
+++ keras/nn/run_100.sh
@@ -1,1 +1,1 @@
-nohup sh -c 'python openpose_100.py > ./log/100ctg_v2/s10/d3dr2_500.txt; echo "nn/100ctg_v2/d3dr2_500の処理が正常に完了したよ" | bash ~/work/slack.sh' &
+nohup sh -c 'python openpose_100.py > ./log/100ctg_v2/s10_v2/d3both_500.txt; echo "nn/100ctg_v2/d3both_500の処理が正常に完了したよ" | bash ~/slack.sh' &
